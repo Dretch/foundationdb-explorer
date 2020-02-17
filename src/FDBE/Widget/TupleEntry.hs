339eb01460b51e04d01a8eb732a99ba964cf2952
Gareth Daniel Smith <garethdanielsmith@gmail.com>
2020-02-17 11:41:25 +0000

    Allow searching by UUID tuple elements.

--- src/FDBE/Widget/TupleEntry.hs
+++ src/FDBE/Widget/TupleEntry.hs
@@ -12,13 +12,17 @@ import           Data.ByteString            (ByteString)
 import           Data.List.Extra            (snoc)
 import           Data.List.Index            (deleteAt, setAt)
 import           Data.Text                  (Text)
+import qualified Data.UUID                  as UUID
 import qualified Data.Vector                as Vector
+import           Data.Word                  (Word32)
 import           FoundationDB.Layer.Tuple   (Elem)
 import qualified FoundationDB.Layer.Tuple   as LT
+import           GI.Gdk                     (EventFocus)
 import           GI.Gtk                     (Align (..), Box (..), Button (..),
                                              Entry (..), Label (..),
                                              Orientation (..), entryGetText)
 import           GI.Gtk.Declarative
+import           Util                       (uncurry4)
 
 import           FDBE.Bytes                 (bytesToText, textToBytes)
 import           FDBE.Widget.ComboBoxBool   (comboBoxBool)
@@ -84,7 +88,7 @@ tupleEntry' elems onChange =
             ]
       where
         combo = comboBoxText []
-          ["None", "Bytes", "Text", "Int", "Float", "Double", "Bool", "Tuple"]
+          ["None", "Bytes", "Text", "Int", "Float", "Double", "Bool", "UUID", "Tuple"]
           (Just position)
           (Just onElemTypeChange)
 
@@ -102,10 +106,25 @@ tupleEntry' elems onChange =
           LT.Float f        -> (4, floatInput f)
           LT.Double d       -> (5, doubleInput d)
           LT.Bool b         -> (6, boolInput b)
-          LT.UUID _ _ _ _   -> (2, textInput "")
+          LT.UUID a b c d   -> (7, uuidInput a b c d)
           LT.CompleteVS _   -> (2, textInput "")
           LT.IncompleteVS _ -> (2, textInput "")
-          LT.Tuple t        -> (7, tupleInput t)
+          LT.Tuple t        -> (8, tupleInput t)
+
+        onElemTypeChange typePos =
+          let newField =
+                case typePos of
+                  0 -> LT.None
+                  1 -> LT.Bytes ""
+                  2 -> LT.Text ""
+                  3 -> LT.Int 0
+                  4 -> LT.Float 0
+                  5 -> LT.Double 0
+                  6 -> LT.Bool False
+                  7 -> LT.UUID 0 0 0 0
+                  8 -> LT.Tuple []
+                  _ -> error "invalid tuple element type position"
+            in onChange $ setAt i newField elems
 
         noneInput =
           widget Label []
@@ -132,29 +151,36 @@ tupleEntry' elems onChange =
 
         floatInput f =
           doubleSpinner [] (realToFrac f) (onElemValueChange . LT.Float . realToFrac)
-        
+
         doubleInput d =
           doubleSpinner [] d (onElemValueChange . LT.Double)
 
         intInput x =
           integerSpinner [] True x (onElemValueChange . LT.Int)
 
+        uuidInput :: Word32 -> Word32 -> Word32 -> Word32 -> Widget event
+        uuidInput a b c d =
+          widget Entry
+            [ #overwriteMode := True
+            , #text := UUID.toText uuid
+            , onM #focusOutEvent onFocusOut
+            ]
+          where
+            uuid = UUID.fromWords a b c d
+            onFocusOut :: EventFocus -> Entry -> IO (Bool, event)
+            onFocusOut _ entry = do
+              text <- #getText entry
+              newUuid <- case UUID.fromText text of
+                Just uuid' ->
+                  pure uuid'
+                Nothing -> do
+                  -- the user entered an invalid UUID, so don't save it
+                  #setText entry $ UUID.toText uuid
+                  pure uuid
+              pure (False, onElemValueChange . uncurry4 LT.UUID . UUID.toWords $ newUuid)
+
         tupleInput t =
           tupleEntry' t (onElemValueChange . LT.Tuple)
-
-        onElemTypeChange typePos =
-          let newField =
-                case typePos of
-                  0 -> LT.None
-                  1 -> LT.Bytes ""
-                  2 -> LT.Text ""
-                  3 -> LT.Int 0
-                  4 -> LT.Float 0
-                  5 -> LT.Double 0
-                  6 -> LT.Bool False
-                  7 -> LT.Tuple []
-                  _ -> error "invalid tuple element type position"
-           in onChange $ setAt i newField elems
 
         onElemValueChange newField =
           onChange $ setAt i newField elems
