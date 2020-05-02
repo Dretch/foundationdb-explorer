3f3f2c1b0107aef5a5346df1cc597f48b94392ac
Gareth Daniel Smith <garethdanielsmith@gmail.com>
2020-05-02 10:32:31 +0100

    Use the icon for the GTK wndows.
    Previously it was just used for the app launcher.
    
    This also upgrades to the latest gi-gtk-declarative (custom attribute branch)

--- app/Main.hs
+++ app/Main.hs
@@ -5,27 +5,37 @@ module Main where
 
 import           FDBE.Prelude
 
-import           Control.Concurrent.Async      (async)
-import           Data.FileEmbed                (embedFile)
-import qualified FoundationDB                  as FDB
-import qualified GI.Gdk                        as Gdk
-import qualified GI.Gtk                        as Gtk
-import           GI.Gtk.Declarative.App.Simple (runLoop)
+import qualified Control.Concurrent.Async                    as Async
+import           Control.Exception                           (finally)
+import           Data.FileEmbed                              (embedFile)
+import qualified FoundationDB                                as FDB
+import qualified GI.Gdk                                      as Gdk
+import qualified GI.Gtk                                      as Gtk
+import           GI.Gtk.Declarative.App.Simple               (runLoop)
+import           GI.Gtk.Declarative.Attributes.Custom.Window (IconData (..),
+                                                              setDefaultIcon)
 
-import           FDBE.App                      (app)
+import           FDBE.App                                    (app)
 
 styles :: ByteString
 styles = $(embedFile "styles.css")
 
+icon :: IconData
+icon = IconDataBytes $(embedFile "icon.png")
+
 main :: IO ()
 main = do
-  void $ Gtk.init Nothing
+  void (Gtk.init Nothing)
+  setDefaultIcon icon
+  setupStyleSheet
+  main' <- Async.async Gtk.main
+  FDB.withFoundationDB FDB.defaultOptions (void . runLoop . app)
+    `finally` (Gtk.mainQuit >> Async.wait main')
+
+setupStyleSheet :: IO ()
+setupStyleSheet = do
   screen <- maybe (fail "no screen!?") pure =<< Gdk.screenGetDefault
   p <- Gtk.cssProviderNew
   Gtk.cssProviderLoadFromData p styles
   let priority = fromIntegral Gtk.STYLE_PROVIDER_PRIORITY_USER
   Gtk.styleContextAddProviderForScreen screen p priority
-  void . async $ do
-    FDB.withFoundationDB FDB.defaultOptions $ void . runLoop . app
-    Gtk.mainQuit
-  Gtk.main
